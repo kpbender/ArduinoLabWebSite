4e2d97051e6543511da5f1a531f58ba283d5ccf6
Krzysztof Bender <benderkp@gmail.com>
2017-12-09 00:07:37 +0000

    Add project files.

---
+++ ArduinoLabWebSite/App_Code/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ArduinoLabWebSite.Startup))]
+namespace ArduinoLabWebSite
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
